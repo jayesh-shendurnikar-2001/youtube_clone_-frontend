8f57124e320489de70969f0da01370041c6b1163
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-03-03 19:03:37 +0530

    add loading spinner to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,28 @@
 
 import VideoCard from "../components/VideoCard.jsx";
 import FilterButtons from "../components/FilterButtons.jsx";
+import { useState } from "react";
 
 const Home = () => {
   const dummyVideos = [];
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className="max-w-7xl mx-auto px-4" id="home-page">
       
+      {loading ? (
+  <div className="flex items-center justify-center min-h-[200px] w-full">
+    <div className="w-10 h-10 border-4 border-gray-700 border-t-red-600 rounded-full animate-spin"></div>
+  </div>
+) : (
+  <p className="text-center text-gray-400 py-20">
+    Videos will appear here
+  </p>
+)}
+      
       <FilterButtons />
+
+
 
       <div className="grid gap-x-4 gap-y-10 
                       grid-cols-1 
